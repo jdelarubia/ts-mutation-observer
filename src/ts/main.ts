182b573a45bd8962e40b6e3a496496cddff6c4de
jdelarubia <jdelarubia@gmail.com>
2022-01-12 23:05:41 +0100

    demo code

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,5 +1,54 @@
 /**
  * main.ts
+ * Main app.
  */
 
-console.log("rolling...");
+import { ToggleEvent, isElementVisible } from "./helpers";
+
+function initToggleAllBtn(parentDiv: HTMLDivElement) {
+  const toggleBtn = <HTMLButtonElement>(
+    document.querySelector("#toggleAllVisibility")
+  );
+  toggleBtn.addEventListener("click", (ev: Event) => {
+    const children = <HTMLCollection>parentDiv.children;
+    for (let i = 0; i < children.length; i++) {
+      children[i].dispatchEvent(ToggleEvent);
+    }
+  });
+} //.
+
+function initToggleChildren(parentDiv: HTMLDivElement) {
+  const children = <HTMLCollection>parentDiv.children;
+  for (let i = 0; i < children.length; i++) {
+    const child = <HTMLDivElement>children[i];
+    child.addEventListener("ToggleVisibility", (ev: Event) => {
+      child.style.visibility = isElementVisible(child) ? "hidden" : "visible";
+    });
+    child.addEventListener("click", (ev: Event) => {
+      child.style.visibility = isElementVisible(child) ? "hidden" : "visible";
+    });
+  }
+} //.
+
+// **********************************************
+// * MutationObserver Demo
+// **********************************************
+function mutationObserverCallback(record: any) {
+  console.log("attributes:", record);
+  console.log("parent:", parentDiv);
+} //.
+
+const parentDiv = <HTMLDivElement>document.querySelector(".parent");
+const observer = new MutationObserver(mutationObserverCallback);
+const observerInit = <MutationObserverInit>{
+  subtree: true,
+  childList: false,
+  attributeOldValue: true,
+  attributes: true,
+  attributeFilter: ["style"],
+};
+observer.observe(parentDiv, observerInit);
+
+// Setup our elements
+initToggleAllBtn(parentDiv);
+initToggleChildren(parentDiv);
